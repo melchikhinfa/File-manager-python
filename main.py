4c7256b5753a391a96c1cbd1761fa593a79d917a
Andrey Melchikhin <amelchikhin@gmail.com>
2023-02-28 19:05:40 +0300

    release version

--- main.py
+++ main.py
@@ -13,35 +13,27 @@ def main():
     os.chdir(WORKING_DIR)
     print(
         "Создана рабочая директория:" + str(WORKING_DIR) +
-        "\nВы можете изменить рабочую директорию "
+        "\nВы можете изменить рабочую директорию в файле settings.py"
     )
 
     while True:
+        try:
+            command = input("\nВведите команду -> ").split(" ")
+            # Остановка работы программы
+            if command[0] == "exit":
+                break
+            # Получаем комманду
+            result = file_processing.router(command[0])
+            if result:
+                try:
+                    result(*command[1:])
+                except TypeError:
+                    print(f"Команда {command[0]} была вызвана с некорректными аргументами")
 
-        command = input("\nВведите команду -> ").split(" ")
-
-        # Остановка работы программы
-        if command[0] == "exit":
-            break
-
-        # Получаем результат существования команды
-        result = file_processing.router(command[0])
-        # Если есть такая команда
-        if result:
-            try:
-                result(*command[1:])
-            except TypeError:
-                print(f"Команда {command[0]} была вызвана с некорректными аргументами")
-
-        else:
-            commands_str = "\n".join(
-                [
-                    f"{key} - {value}"
-                    for (key, value) in FileManager.get_commands().items()
-                ]
-            )
-            print(f"Команда {command[0]} не найдена! Список команд:\n{commands_str}")
-
+            else:
+                print(f"Команда {command[0]} не найдена! \nВведите команду manual для вывода справки по командам.")
+        except KeyboardInterrupt:
+            print("Произведен выход из программы.")
     print("Произведен выход из программы.")
 
 
